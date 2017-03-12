0620c99bafe467c8a40e333dbed59a95da77a4b6
Anuradha Bhatt <anudave21@gmail.com>
2017-03-12 13:18:43 -0500

    Edit and Update commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,69 @@
 class ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+    render "show_all_products.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    product = Product.new(
+      name: params[:form_name],
+      price: params[:form_price],
+      image: params[:form_image],
+      description: params[:form_description]
+    )
+    product.save
+    render "create.html.erb"
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "edit.html.erb"
+  end
+
+  def update
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.name = params[:form_name]
+    @product.price = params[:form_price]
+    @product.image = params[:form_image]
+    @product.description = params[:form_description]
+    @product.save
+    render "update.html.erb"
+  end
+
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    # @product = Product.name
+    render "show_products.html.erb"
+  end
+
   def all_products
     @show_products = Product.all
     render "show_all_products.html.erb"
   end
+  
   def product_1
     @product1_image = Product.all 
     render "product1_image_link.html.erb"
   end
+
+  def destroy
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    # @product.name = params[:form_name]
+    # @product.price = params[:form_price]
+    # @product.image = params[:form_image]
+    # @product.description = params[:form_description]
+    @product.destroy
+    render "destroy.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get "/products" => "products#index"
   get "/products_url" => "products#all_products"
   get "/product1_link" => "products#product_1"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
+  get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
+  
+  get "/products/:id" => "products#show"
+  # get "/products/:name" => "products#show"
+  delete "/products/:id" => "products#destroy"
 end
